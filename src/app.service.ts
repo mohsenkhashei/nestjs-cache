5482ebc4d85af9b53623595da2ac008f0592120f
MohsenKhashei <m.khasheii@gmail.com>
2023-06-08 18:01:17 +0300

    test service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(@Inject(CACHE_MANAGER) private readonly cacheManager: Cache) {}
+
+  async getHello() {
+    await this.cacheManager.set('cached_item', 'test', 10);
+    await this.cacheManager.del('cached_item');
+    await this.cacheManager.reset();
+    const cachedItem = await this.cacheManager.get('cached_item');
+    console.log(cachedItem);
+
     return 'Hello World!';
   }
 }
