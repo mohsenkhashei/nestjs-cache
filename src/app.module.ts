c3bd960736e110aaff965f109ec9baded74df3e9
MohsenKhashei <m.khasheii@gmail.com>
2023-06-08 18:00:54 +0300

    register cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Module } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      // ttl: 60, last for
+      // max: 1000, items before cache empties
+      isGlobal: true,
+    }),
+  ],
+
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR, // caching whole module
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
